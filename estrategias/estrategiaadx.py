97413b8b32733398f1815a1a5276ef514dbad977
luisomar3 <luisomar3@hotmail.com>
2018-05-31 21:33:28 -0400

    a

--- estrategias/estrategiaadx.py
+++ estrategias/estrategiaadx.py
@@ -1,24 +1,3 @@
-<<<<<<< HEAD
-import pandas as pd 
-import numpy as np 
-
-from config import config
-from estrategias.basestrategy import BaseStrategy
-from indicadores import *
-
-indicadores = Indicadores()
-
-class EstrategiaAdx(BaseStrategy):
-    
-    def PDI_NDI_Cossover(self,dataFrame):
-        """ Estrategia Crossover del ADX, en realidad, usaremos los NDI y PDI para generar las señales de compra/venta
-
-        """
-        datos = indicadores.ADX(dataFrame)
-        datos = datos.apply(self.crossover,args = ('PDI','NDI'),axis = 1)
-        
-        return datos
-=======
 import pandas as pd 
 import numpy as np 
 
@@ -158,4 +137,3 @@ class EstrategiaAdx(BaseStrategy):
             plt.show()
         
         return averages
->>>>>>> taIncorporation
